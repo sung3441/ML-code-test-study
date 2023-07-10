392f93610d38f55cc87365cd446c3eb51917f8b2
JaeHunJaeHun <jhl9838@gmail.com>
2023-07-10 23:13:09 +0900

    update : 코딩테스트 Template setting

---
+++ Programmers/Level-1/jaehunlee/Template.java
@@ -0,0 +1,12 @@
+public class Template {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+    }
+    static class Solution {
+        public int solution(int n, int[] lost, int[] reserve) {
+            int answer = 0;
+            return answer;
+        }
+    }
+}
