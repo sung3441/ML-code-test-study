918456b9f1802248b408f1bb48fa3e59f64abe05
khc <picasso5536@gmail.com>
2023-07-17 23:39:19 +0900

    lifeboat

--- hoecheol/programmers/level2/구명보트.java
+++ hoecheol/programmers/level2/구명보트.java
@@ -3,32 +3,17 @@ import java.util.*;
 class 구명보트 {
     public int solution(int[] people, int limit) {
         int answer = 0;
-        int cnt = 0;
+        int min = 0;
 
         Arrays.sort(people);
 
-        for(int i = 0; i < people.length; i++){
-            if(people[i] > limit - people[0]){
+        for (int i = people.length - 1; i >= min; i--) {
+            if (people[i] + people[min] <= limit) {
+                min++;
                 answer++;
-                people[i] = -1;
-            }else{
-                cnt++;
+            } else {
+                answer++;
             }
-        }
-
-        for(int i = 0; i < people.length; i++){
-            for(int j = people.length-1; j > i; j--){
-                if(people[i] != -1 && people[j] != -1){
-                    if(people[i] + people[j] <= limit){
-                        answer++;
-                        people[i] = -1;
-                        people[j] = -1;
-                    }
-                }
-            }
-        }
-        if(cnt % 2 != 0){
-            answer++;
         }
 
         return answer;
