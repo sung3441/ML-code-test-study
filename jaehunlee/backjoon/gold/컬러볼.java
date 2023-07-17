e597dc4685320587fb97671ff6922ace5f01107c
JaeHunJaeHun <jhl9838@gmail.com>
2023-07-18 00:44:32 +0900

    temp commit

--- jaehunlee/backjoon/gold/컬러볼.java
+++ jaehunlee/backjoon/gold/컬러볼.java
@@ -1,23 +1,52 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class 컬러볼 {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int a = Integer.valueOf(st.nextToken());
-        int[] balls = new int[a*2];
-
+        List<Ball> ballList = new ArrayList<>();
         for (int i = 0; i < a; i++) {
-            for (int j = 0; j < 2; j++) {
-                st = new StringTokenizer(br.readLine());
-                balls[i++] = Integer.valueOf(st.nextToken());
-            }
+            Ball ball = new Ball();
+            st = new StringTokenizer(br.readLine());
+            ball.setNum(Integer.valueOf(st.nextToken()));
+            ball.setSize(Integer.valueOf(st.nextToken()));
+            ballList.add(ball);
+        }
+    }
+
+    static class Ball {
+        private int num;
+        private int size;
+
+        @Override
+        public String toString() {
+            return "Ball{" +
+                    "num=" + num +
+                    ", size=" + size +
+                    '}';
         }
 
+        public int getNum() {
+            return num;
+        }
 
+        public void setNum(int num) {
+            this.num = num;
+        }
+
+        public int getSize() {
+            return size;
+        }
+
+        public void setSize(int size) {
+            this.size = size;
+        }
     }
 }
+
