eca2e46e03c676a2762f3d5ba1eb318171c02e5f
mlsoft <ljh0901@mlsoft.com>
2023-07-18 14:00:58 +0900

    temp commit

--- jaehunlee/backjoon/gold/컬러볼.java
+++ jaehunlee/backjoon/gold/컬러볼.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class 컬러볼 {
     public static void main(String[] args) throws Exception {
@@ -12,41 +10,60 @@ public class 컬러볼 {
         int a = Integer.valueOf(st.nextToken());
         List<Ball> ballList = new ArrayList<>();
         for (int i = 0; i < a; i++) {
-            Ball ball = new Ball();
             st = new StringTokenizer(br.readLine());
-            ball.setNum(Integer.valueOf(st.nextToken()));
-            ball.setSize(Integer.valueOf(st.nextToken()));
-            ballList.add(ball);
+            int color = Integer.valueOf(st.nextToken());
+            int size = Integer.valueOf(st.nextToken());
+            ballList.add(new Ball(color, size, 0));
+            int zero = 0;
+            for (int j = zero; j < ballList.size(); j++) {
+                Ball ball = ballList.get(j);
+                if (ball.getColor() != color && ball.getSize() > size) {
+                    ball.setCatchValue(ball.getCatchValue() + size);
+                }
+            }
         }
+        for (int i = 0; i < ballList.size(); i++) {
+            System.out.println(ballList.get(i).getCatchValue());
+        }
+
     }
 
     static class Ball {
-        private int num;
-        private int size;
+        int color;
+        int size;
+        int catchValue;
 
         @Override
         public String toString() {
             return "Ball{" +
-                    "num=" + num +
+                    "color=" + color +
                     ", size=" + size +
+                    ", catchValue=" + catchValue +
                     '}';
         }
 
-        public int getNum() {
-            return num;
+        Ball(int color, int size, int catchValue) {
+            this.color = color;
+            this.size = size;
+            this.catchValue = catchValue;
         }
 
-        public void setNum(int num) {
-            this.num = num;
+        public int getColor() {
+            return color;
         }
 
         public int getSize() {
             return size;
         }
 
-        public void setSize(int size) {
-            this.size = size;
+        public int getCatchValue() {
+            return catchValue;
+        }
+
+        public void setCatchValue(int catchValue) {
+            this.catchValue = catchValue;
         }
     }
+
 }
 
