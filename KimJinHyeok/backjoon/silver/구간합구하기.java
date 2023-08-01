a83987db27864ac0e0589647fb9602b06b34385b
ManGO <will4981@naver.com>
2023-08-01 13:28:05 +0900

    Update 구간합구하기.java

--- KimJinHyeok/backjoon/silver/구간합구하기.java
+++ KimJinHyeok/backjoon/silver/구간합구하기.java
@@ -8,17 +8,17 @@ public class Main {
 		int m = sc.nextInt();
 		
 		int[] nList = new int[n+1];
-    	int[] nSumList = new int[n+1];
+    		int[] nSumList = new int[n+1];
     	
-    	for(int i =1;i<=n;i++) {
-    		nSumList[i] = nSumList[i-1] + sc.nextInt();
-    	}
-		
-    	for(int i=0;i<m;i++) {
-    		int start = sc.nextInt();
-    		int end =sc.nextInt();
-    		
-    		System.out.println(nSumList[end] - nSumList[start-1]);
+	    	for(int i =1;i<=n;i++) {
+	    		nSumList[i] = nSumList[i-1] + sc.nextInt();
+	    	}
+			
+	    	for(int i=0;i<m;i++) {
+	    		int start = sc.nextInt();
+	    		int end =sc.nextInt();
+	    		
+	    		System.out.println(nSumList[end] - nSumList[start-1]);
 		}
 	}
 }
